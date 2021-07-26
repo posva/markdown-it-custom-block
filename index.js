e4c244c34252685c42e0f3111ab21478fd1f4315
Eduardo San Martin Morote <posva13@gmail.com>
2021-07-26 12:22:41 +0200

    fix: handle multilines in argument
    Fix #46

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const embedRE = /@\[([\w-]+)\]\((.+)\)/im
+const embedRE = /@\[([\w-]+)\]\(([\s\S]+)\)/im
 
 module.exports = function plugin (md, options) {
   md.renderer.rules.custom = function tokenizeBlock (tokens, idx) {
